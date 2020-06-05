e29063584add929bb48450d034f8f11c7a3562af
Andric LibreSinn <andric.droid@gmail.com>
2020-06-05 16:12:04 -0400

    Empty methods will now use empty parens

--- lessons/lesson09/index.js
+++ lessons/lesson09/index.js
@@ -1,50 +1,49 @@
-/* eslint-disable no-unused-vars */
+/* ceslint-disable no-unused-vars */
 
 import { List } from 'immutable-ext'
-import * as lesson from '../lesson08'
 import { Right, Left } from '../common'
 
 // The max of all numbers.
 export const Max = x => ({
   x,
   concat: ({ x: y }) => Max(x > y ? x : y),
-  inspect: _ => `Max(${x})`
+  inspect: () => `Max(${x})`
 })
 
 // Multiply together.
 export const Product = x => ({
   x,
   concat: ({ x: y }) => Product(x * y),
-  inspect: _ => `Product(${x})`
+  inspect: () => `Product(${x})`
 })
 
 // Take anything (first thing) that has a value.
 export const Any = x => ({
   x,
   concat: ({ x: y }) => Any(x || y),
-  inspect: _ => `Any(${x})`
+  inspect: () => `Any(${x})`
 })
 
 // Take anything (first thing) that has a value.
 export const Min = x => ({
   x,
   concat: ({ x: y }) => Min(x < y ? x : y),
-  inspect: _ => `Min(${x})`
+  inspect: () => `Min(${x})`
 })
 
 // Wrap Either into First
 export const First = either => ({
   fold: f => f(either),
   concat: o => either.isLeft ? o : First(either),
-  inspect: _ => `First(${either.inspect()})`
+  inspect: () => `First(${either.inspect()})`
 })
 
 // All the empty methods together so that they are easier to see.
-Max.empty = _ => Max(-Infinity)
-Any.empty = _ => Any(false)
-Min.empty = _ => Any(Infinity)
-Product.empty = _ => Product(1)
-First.empty = _ => First(Left())
+Max.empty = () => Max(-Infinity)
+Any.empty = () => Any(false)
+Min.empty = () => Any(Infinity)
+Product.empty = () => Product(1)
+First.empty = () => First(Left())
 
 export const find = (xs, f) => List(xs).foldMap(
   x => First(f(x) ? Right(x) : Left()),
