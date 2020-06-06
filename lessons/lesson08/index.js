f60552c017990c0d40a0c2125cfae5540548f298
Andric LibreSinn <andric.droid@gmail.com>
2020-06-05 20:52:44 -0400

    Fixing linting errors

--- lessons/lesson08/index.js
+++ lessons/lesson08/index.js
@@ -1,27 +1,25 @@
-/* eslint-disable no-unused-vars */
-
 export const Sum = x => ({
   x,
   concat: ({ x: y }) => Sum(x + y),
-  inspect: _ => `Sum(${x})`,
+  inspect: () => `Sum(${x})`,
 })
 
 export const All = x => ({
   x,
   concat: ({ x: y }) => All(x && y),
-  inspect: _ => `All(${x})`,
+  inspect: () => `All(${x})`,
 })
 
 export const First = x => ({
   x,
-  concat: _ => First(x),
-  inspect: _ => `First(${x})`,
+  concat: () => First(x),
+  inspect: () => `First(${x})`,
 })
 
 // hmmm, no way to define a neutral element that is first and has no effect
 // seems like this must remain a semigroup instead of a monoid
-// First.empty = _ => First(???)
+// First.empty = () => First(???)
 
 // I hate this syntax so much. Grouping them so that the above is cleaner.
-Sum.empty = _ => Sum(0)
-All.empty = _ => All(true)
+Sum.empty = () => Sum(0)
+All.empty = () => All(true)
