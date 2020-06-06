3a17597ca4e3f0d4bc62890d55881843c9f87816
Andric LibreSinn <andric.droid@gmail.com>
2020-06-05 20:41:37 -0400

    Finished lesson 03

--- lessons/lesson03/index.js
+++ lessons/lesson03/index.js
@@ -1,55 +1,19 @@
-console.clear()
-
-const Right = (x) => ({
+export const Right = (x) => ({
   map: (f) => Right(f(x)),
   fold: (f, g) => g(x),
   inspect: () => `Right(${x})`,
 })
 
-// Right() is the happy path. If you set up Right() with a value then map over
-// it you'll apply the function proficed as such in the example:
-const rightTest = Right(3).map(x => x + 1)
-console.log(rightTest)
-console.log()
-
-const Left = (x) => ({
+export const Left = (x) => ({
   map: (f) => Left(x),
   fold: (f, g) => f(x),
   inspect: () => `Left(${x})`,
-});
+})
 
-// Left() doesn't run the function in map. Meaning that if you call fold()
-// on the value you'll just get the result back. For example:
-const leftTest = Left(3).map(x => x + 1)
-console.log(leftTest)
-console.log()
+export const fromNullable = (x) => x != null ? Right(x) : Left(null)
 
-const fromNullable = (x) => x != null ? Right(x) : Left(null)
-const resultGood = Right(2).map(x => x + 1).map(x => x / 2).fold(x => 'error', x => x)
-const resultError = Left(2).map(x => x + 1).map(x => x / 2).fold(x => 'error', x => x)
-
-console.log('Good result returns:', resultGood)
-console.log('Bad result returns:', resultError)
-console.log()
-
-const findColor = (name) => fromNullable(({
+export const findColor = (name) => fromNullable(({
   red: '#ff4444',
   blue: '#3b5998',
   yellow: '#fff68f',
 })[name])
-
-// findColor('123').fold(console.log)
-// findColor('red').fold(null, console.log)
-
-const result1 = findColor('red')
-  .map(c => c.slice(1))
-  .fold(
-    // e => Either
-    e => 'no color',
-
-    // o => Or
-    o => o.toUpperCase()
-  )
-
-console.log(result1)
-
--- lessons/lesson03/index.spec.js
+++ lessons/lesson03/index.spec.js
@@ -1,12 +1,70 @@
 import chai, { expect } from 'chai'
 import * as lesson from './index.js'
+import { describe } from 'mocha'
 
 chai.should()
 
+const err = e => e
+const identity = i => i
+
 describe('Lesson 03 - Enforce a null check with composable code branching using Either', () => {
-  describe('bar', () => {
-    it('baz', () => {
-      expect(1).to.equal(1)
+  describe('Right() & Left() test', () => {
+    const { Right, Left } = lesson
+
+    it('Right() test', () => {
+      // Right() is the happy path. If you set up Right() with a value then map over
+      // it you'll apply the function proficed as such in the example:
+      const rightTest = Right(3).map(x => x + 1)
+
+      expect(rightTest.fold(err, identity)).to.equal(4)
+    })
+
+    it('Left() test', () => {
+      // Left() doesn't run the function in map. Meaning that if you call fold()
+      // on the value you'll just get the result back. For example:
+      const leftTest = Left(3).map(x => x + 1)
+
+      expect(leftTest.fold(err, identity)).to.equal(3)
+    })
+
+    it('Happy path: multimap to value', () => {
+      const resultGood = Right(2).map(x => x + 1).map(x => x / 2).fold(x => 'error', x => x)
+      expect(resultGood).to.equal(1.5)
+    })
+    
+    it('Unhappy path: multimap to value', () => {
+      const resultError = Left(2).map(x => x + 1).map(x => x / 2).fold(x => 'error', x => x)
+      expect(resultError).to.equal('error')
+    })
+  })
+
+  describe('fromNullable() returns an Either() (Right/Left)', () => {
+    const { fromNullable, findColor } = lesson
+
+    it('test out the regular findColor() function; it returns null or string', () => {
+      const a = findColor('123').fold(err, identity)
+      const b = findColor('red').fold(err, identity)
+
+      expect(a).to.equal(null)
+      expect(b).to.equal('#ff4444')
+    })
+
+    describe('Now to handle a null case so we can always return a string value', () => {
+      it('If a function returns a Right() then we get the processed value', () => {
+        const val = findColor('red')
+          .map(c => c.slice(1)) // removes the '#'
+          .fold(e => 'no color', o => o.toUpperCase())
+
+        expect(val).to.equal('FF4444')
+      })
+
+      it('If a function returns a left() then we get the default error value', () => {
+        const val = findColor('123')
+          .map(c => c.slice(1)) // removes the '#'
+          .fold(e => 'no color', o => o.toUpperCase())
+
+        expect(val).to.equal('no color')
+      })
     })
   })
 })
