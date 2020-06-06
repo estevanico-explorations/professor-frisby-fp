9a196fa66605612db6115a4d8467825dbaf2f519
Andric LibreSinn <andric.droid@gmail.com>
2020-06-05 20:21:49 -0400

    Finishing lesson 4

--- lessons/lesson02/index.js
+++ lessons/lesson02/index.js
@@ -1,20 +1,17 @@
-var lesson02;
-(function (lesson02) {
-    const Box = (x) => ({
-        map: (f) => Box(f(x)),
-        fold: (f) => f(x),
-        inspect: () => `Box(${x})`
-    });
-    const moneyToFloat = (str) => Box(str.replace(/\$/g, ''))
-        .fold(parseFloat);
-    const percentToFloat = (str) => Box(str.replace(/\%/g, ''))
-        .map(parseFloat)
-        .fold(n => n * 0.01);
-    const applyDiscount = (price, discount) => Box(moneyToFloat(price))
-        .fold(cost => Box(percentToFloat(discount))
-        .fold(savings => cost - cost * savings));
-    console.log(moneyToFloat('$100'));
-    console.log(percentToFloat('10%'));
-    console.log(applyDiscount('$5.00', '20%'));
-    console.log(applyDiscount('$200', '20%'));
-})(lesson02 || (lesson02 = {}));
+
+const Box = (x) => ({
+  map: (f) => Box(f(x)),
+  fold: (f) => f(x),
+  inspect: () => `Box(${x})`
+})
+
+export const moneyToFloat = (str) => Box(str.replace(/\$/g, ''))
+  .fold(parseFloat)
+
+export const percentToFloat = (str) => Box(str.replace(/\%/g, ''))
+  .map(parseFloat)
+  .fold(n => n * 0.01)
+
+export const applyDiscount = (price, discount) => Box(moneyToFloat(price))
+  .fold(cost => Box(percentToFloat(discount))
+  .fold(savings => cost - cost * savings))
--- lessons/lesson02/index.spec.js
+++ lessons/lesson02/index.spec.js
@@ -1,12 +1,19 @@
 import chai, { expect } from 'chai'
-import * lesson from './index.js'
+import { moneyToFloat, percentToFloat, applyDiscount } from './index.js'
 
 chai.should()
 
 describe('Lesson 02 - Refactor imperative code to a single composed expression using Box', () => {
-  describe('bar', () => {
-    it('baz', () => {
-      expect(1).to.equal(1)
-    })
+  it('moneyToFloat -> Box()', () => {
+    expect(moneyToFloat('$100')).to.equal(100)
+  })
+  
+  it('percentToFloat -> Box()', () => {
+    expect(percentToFloat('10%')).to.equal(.1)
+  })
+
+  it('applyDiscount -> Box()', () => {
+    expect(applyDiscount('$5.00', '20%')).to.equal(4)
+    expect(applyDiscount('$200', '20%')).to.equal(160)
   })
 })
