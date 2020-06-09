941ce836a4ba4cf5298e85efd1219ab261638958
Andric LibreSinn <andric.libresinn@libertymutual.com>
2020-06-09 14:31:32 -0400

    Adding spec file back

---
+++ lessons/lesson15/index.spec.js
@@ -0,0 +1,10 @@
+import chai from 'chai'
+
+chai.should()
+
+describe('Lesson 16 - Lift into a Pointed Functor with of', () => {
+  describe('There is no code nor test file to run so just go ahead and read the readme and move on. ', () => {
+    it('love you', () => {
+    })
+  })
+})
