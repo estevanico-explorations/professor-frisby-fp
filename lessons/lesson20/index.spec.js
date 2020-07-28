34ab4eb2e0a3e2828849d5ecdffe0f8fa6a3affd
Andric LibreSinn <andric.libresinn@libertymutual.com>
2020-07-28 18:01:38 -0400

    Fixing lesson 20

--- lessons/lesson20/index.spec.js
+++ lessons/lesson20/index.spec.js
@@ -1,23 +1,18 @@
 import chai, { expect } from 'chai'
-import * as lesson from './index.js'
+import { List } from 'immutable-ext'
 
 chai.should()
 
 describe('[UNFINISHED] Lesson 20 - List comprehensions with Applicative Functors', () => {
-  describe('Original state', () => {
-    it('it', () => {
-    })
-  })
-
-  describe('Middle state', () => {
-    describe('another thing', () => {
-      it('it', () => {
-      })
-    })
-  })
-
-  describe('Final state', () => {
-    it('it', () => {
-    })
+  it('it', () => {
+    const merch = () =>
+    List.of(x => y => `${x} ${y}`)
+    .ap(List(['teeshirt', 'sweater']))
+    .ap(List(['S', 'M', 'L']))
+    
+    const result = merch().toString()
+    const expected = 'List [ "teeshirt S", "teeshirt M", "teeshirt L", "sweater S", "sweater M", "sweater L" ]'
+    
+    expect(result).to.equal(expected)
   })
 })
